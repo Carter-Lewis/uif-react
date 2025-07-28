bbd55cab94fa7f0f648e00555f776688fed3d3d0
Carter Lewis <carter_lewis1@baylor.edu>
2025-07-28 18:11:14 -0500

    fix minor bugs and formatting in aboutcards

--- uif-react/src/Components/About/About.jsx
+++ uif-react/src/Components/About/About.jsx
@@ -10,9 +10,14 @@ const About = () => {
   return (
     <>
     <div className={styles.aboutWrapper} id='about'>
-        <AboutCard title="Who we are">
-
-        </AboutCard>
+        <AboutCard 
+          title="Who we are" 
+          side='left' 
+          content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, aspernatur perspiciatis corrupti doloribus vitae itaque voluptates voluptatibus error vero excepturi.'></AboutCard>
+        <AboutCard 
+          title='What we do' 
+          side='right' 
+          content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae fugit, a quasi perspiciatis laboriosam commodi?' />
     </div>
     </>
   )
--- uif-react/src/Components/About/AboutCard/AboutCard.jsx
+++ uif-react/src/Components/About/AboutCard/AboutCard.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './AboutCard.module.css'
 import gsap from 'gsap'
 import { SplitText, ScrollTrigger } from 'gsap/all'
 import { useGSAP } from '@gsap/react'
 
 const AboutCard = (props) => {
+  const cardRef = useRef(null);
 
   useGSAP(() => {
-    gsap.from('#about-card', {
-      xPercent: -150,
+    gsap.from(cardRef.current, {
+      xPercent: props.side === 'left' ? -150 : 150,
       ease: 'power2.out',
       scrollTrigger: {
-        trigger: '#about-card',
+        trigger: cardRef.current,
         start: 'top center',
         end: 'bottom center',
         scrub: true
       }
     })
-  })
+  }, [props.side]);
 
   return (
-    <div className={styles.aboutCard} id='about-card'>
+    <div className={styles.aboutCard} ref={cardRef}>
+      {props.side==='left'?<><h1 className={styles.cardTitle}>{props.title}</h1>
+        <p className={styles.cardContent}>{props.content}</p></>
+        :
+        <><p className={styles.cardContent}>{props.content}</p>
         <h1 className={styles.cardTitle}>{props.title}</h1>
-        <p className={styles.cardContent}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
+        </>}
+        
     </div>
   )
 }

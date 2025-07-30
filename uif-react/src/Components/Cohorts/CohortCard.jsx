f223d1710bded65255920a5ee73a9704f5390b7f
Carter Lewis <carter_lewis1@baylor.edu>
2025-07-30 18:49:26 -0500

    Implement cohort card - still need to implement member cards to introduce the team

--- uif-react/src/Components/Cohorts/CohortCard.jsx
+++ uif-react/src/Components/Cohorts/CohortCard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "./CohortCard.module.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-const CohortCard = () => {
-  return (
-    <div>CohortCard</div>
-  )
-}
+const CohortCard = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default CohortCard
+  const cardStyle = {
+    backgroundImage: !isOpen ? `url('${props.imageUrl || "/Images/UIF Team.JPG"}')` : 'none',
+    width: isOpen ? 'auto' : '600px',
+    minWidth: isOpen ? '800px' : '600px',
+  };
+
+return (
+    <>
+        <motion.div
+            layout
+            onClick={() => setIsOpen(!isOpen)}
+            className={styles.card}
+            style={cardStyle}
+        >
+            <motion.h2 className={styles.cohortCardTitle}>{props.title}</motion.h2>
+            {!isOpen && <motion.div className={styles.cohortCardContent}>
+                <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </motion.div>}
+            {isOpen && 
+                    <motion.div className={styles.memberCardsWrapper}>
+                            {props.children}
+                    </motion.div>
+            }
+        </motion.div>
+    </>
+);
+};
+
+export default CohortCard;
--- uif-react/src/Components/Cohorts/Cohorts.jsx
+++ uif-react/src/Components/Cohorts/Cohorts.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import styles from './Cohorts.module.css'
+import React from "react";
+import styles from "./Cohorts.module.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import CohortCard from "./CohortCard";
 
 const Cohorts = () => {
-  return (
-    <div className={styles.cohortWrapper} id='cohorts'>Cohorts</div>
-  )
-}
+  useGSAP(() => {
+    gsap.from("#cohorts-title", {
+      scale: 0,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#cohorts-title",
+        start: "bottom bottom",
+        end: "center 25%",
+        scrub: true,
+      },
+    });
+  });
 
-export default Cohorts
+  return (
+    <div className={styles.cohortsWrapper} id="cohorts">
+      <h1 className={styles.cohortsTitle} id="cohorts-title">
+        Cohorts
+      </h1>
+      <div className={styles.cohortsCardsWrapper}>
+        <CohortCard title="Cohort Zero" backgroundImage="/Images/UIF Team.JPG">
+          <img src="/Images/Warren Huang.JPG" alt="Carter Lewis" />
+          <img src="/Images/Carter Lewis.JPG" alt="Carter Lewis" />
+          <img src="/Images/Ethan Friend.JPG" alt="Carter Lewis" />
+          <img src="/Images/Spencer Yim.JPG" alt="Carter Lewis" />
+        </CohortCard>
+      </div>
+    </div>
+  );
+};
+
+export default Cohorts;
